6025f9a1fd55f2230170c9ba89c737aec1999cf2
nidia <nidiacmh@gmail.com>
2024-12-08 00:17:28 -0600

    agregar prueba de validación de precio total negativo

--- src/test/java/org/carolina/descuento/DescuentoTemporadaTest.java
+++ src/test/java/org/carolina/descuento/DescuentoTemporadaTest.java
@@ -3,12 +3,26 @@ package org.carolina.descuento;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DescuentoTemporadaTest {
     @Test
-    void testAplicarDescuento(){
+    void testAplicarDescuento() throws Exception {
         DescuentoTemporada descuento = new DescuentoTemporada();
         double resultado = descuento.aplicarDescuento(100);//aplica un 10% de descuento
         assertEquals(90.0, resultado);
+    }
+
+    @Test
+    void testAplicarDescuento110() throws Exception {
+        DescuentoTemporada descuento = new DescuentoTemporada();
+        double resultado = descuento.aplicarDescuento(110);//aplica un 10% de descuento
+        assertEquals(99.0, resultado);
+    }
+
+    @Test
+    void testAplicarDescuentoMenorCero() {
+        DescuentoTemporada descuento = new DescuentoTemporada();
+        assertThrows(Exception.class, () -> descuento.aplicarDescuento(-1));
     }
 }
